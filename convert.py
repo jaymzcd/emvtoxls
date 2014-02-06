01ca6d839fec591d0d8ae8ff5fe2e3ebd1366dda
jaymz <jaymz@u-dox.com>
2014-02-06 03:30:17 +0000

    add freezing and bold to header row

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+import os
 import sys
 import xlwt
 import time
@@ -12,12 +13,23 @@ def create_xls(name='Sheet 1'):
     ws = wb.add_sheet(name)
     return wb, ws
 
+def get_header_style():
+    font = xlwt.Font()
+    font.bold = True
+    style = xlwt.XFStyle()
+    style.font = font
+    return style
+
 def parse(filename, outfile, nodata=''):
     start = time.clock()
 
     xmlf = minidom.parse(filename)
     members = xmlf.getElementsByTagName('MEMBER')
+    header_style = get_header_style()
     wb, ws = create_xls()
+
+    ws.set_panes_frozen(True)
+    ws.set_horz_split_pos(1)
 
     row_pointer = 0
 
@@ -34,7 +46,7 @@ def parse(filename, outfile, nodata=''):
             offset = 0
 
             if row_pointer == 0:
-                ws.write(row_pointer, col_pointer, fieldname)
+                ws.write(row_pointer, col_pointer, fieldname, header_style)
                 offset = 1  # shift down now for the data row
 
             if tag.firstChild:
@@ -61,6 +73,13 @@ def parse(filename, outfile, nodata=''):
     end = time.clock()
     print "Processed {0} entries in {1:.2f}s".format(row_pointer, end - start)
 
+def check_filesize(filename):
+    size = os.stat(filename).st_size
+    filesize_meg = float(size) / 1024**2
+    if filesize_meg > 5:
+        print "Not yet suitable for large XML files"
+        sys.exit(3)
+
 def run():
     parser = OptionParser()
     parser.add_option("-f", "--file", default='out.xls', dest="filename", help="write output to file.xls", metavar="FILE")
@@ -71,6 +90,7 @@ def run():
         print "You must provide the input XML file"
         sys.exit(2)
 
+    check_filesize(args[0])
     parse(args[0], options.filename, nodata=options.nodata)
 
 
